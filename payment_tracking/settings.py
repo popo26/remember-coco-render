87f07115ebbcc038b7d9f0934ccbe00a705b355a
popo26 <mynameispopo26@gmail.com>
2022-09-29 10:24:57 +1300

    procfile

--- payment_tracking/settings.py
+++ payment_tracking/settings.py
@@ -17,6 +17,8 @@ import dj_database_url
 
 load_dotenv()
 
+#Python 3.7 for Render
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
